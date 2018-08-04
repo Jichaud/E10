8bc96bd0de5a2d07d5e8983e3bbdfaeb47c5cdeb
dell <jich1978@gmail.com>
2018-08-04 17:58:24 -0300

    Add files via Atom

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,14 @@
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 50 ) {
-        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    if ($(this).scrollTop() > 100 ) {
+        $('.scrolltop:hidden').fadeIn();
     } else {
-        $('.scrolltop').stop(true, true).fadeOut();
+        $('.scrolltop').fadeOut();
     }
+    $('.scrolltop').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
 });
 // $(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
