92ea114427dfe5c4682e23f6f02a3d6fbe1153b4
jichaudm <jich1978@gmail.com>
2019-03-17 23:13:01 -0300

    Add files via Visual Studio Code

--- datos.js
+++ datos.js
@@ -7,10 +7,10 @@ firebase.initializeApp({
   // Initialize Cloud Firestore through Firebase
   var pedidoDeDatos = firebase.firestore();
 
-  pedidoDeDatos.collection("ceide").doc("datos-personales").set({
+  pedidoDeDatos.collection("ceide").doc("datos-personales-ceide").set({
     nombre: "Néstor",
     apellido: "Ceide",
-    cuit: "20-07763421-6",
+    cuit: "20-07763421-7",
     nacido: "15/01/1947"
 })
 .then(function() {
@@ -21,17 +21,82 @@ firebase.initializeApp({
 });
 
 // rellenar datos personales
-var docRef = pedidoDeDatos.collection("ceide").doc("datos-personales");
+var docRef = pedidoDeDatos.collection("ceide").doc("datos-personales-ceide");
 
-docRef.get().then(function(doc) {
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-        var nombreF = document.getElementById("nombreF");
-        nombreF.value = nombre;
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");        
-    }
-}).catch(function(error) {
-    console.log("Error getting document:", error);
+docRef.get().then(function(documentSnaposhot){
+if (documentSnaposhot.exists){
+ var data = documentSnaposhot.data().nombre; 
+ document.getElementById("nombreF").value = data;
+ var data = documentSnaposhot.data().apellido; 
+ document.getElementById("apellidoF").value = data;
+} else {
+    console.log('document not found');
+}
 });
+
+// rellenar rodados
+function guardar(){
+    var fecha_alta = document.getElementById("fechaRF").value;
+    var marca = document.getElementById("marcaRF").value;
+    var patente = document.getElementById("patenteRF").value;
+    pedidoDeDatos.collection("ceide").add({
+        tipo: "Rodado",
+        fecha_alta: fecha_alta,
+        marca: marca,
+        patente: patente
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
+
+// OTRO USUARIO
+
+pedidoDeDatos.collection("diez").doc("datos-personales-diez").set({
+    nombre: "Gustavo",
+    apellido: "Diez",
+    cuit: "23-12895122-9",
+    nacido: "03/12/1958"
+})
+.then(function() {
+    console.log("Documento guardado correctamente");
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+
+// rellenar datos personales
+var docRef = pedidoDeDatos.collection("diez").doc("datos-personales-diez");
+
+docRef.get().then(function(documentSnaposhot){
+if (documentSnaposhot.exists){
+ var data = documentSnaposhot.data().nombre; 
+ document.getElementById("nombreF").value = data;
+ var data = documentSnaposhot.data().apellido; 
+ document.getElementById("apellidoF").value = data;
+} else {
+    console.log('document not found');
+}
+});
+
+// rellenar rodados
+function guardar(){
+    var fecha_alta = document.getElementById("fechaRF").value;
+    var marca = document.getElementById("marcaRF").value;
+    var patente = document.getElementById("patenteRF").value;
+    pedidoDeDatos.collection("diez").add({
+        tipo: "Rodado",
+        fecha_alta: fecha_alta,
+        marca: marca,
+        patente: patente
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
