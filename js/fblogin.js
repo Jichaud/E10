77f01ad670176263946bcd1712244060741e260c
jichaud <jich1978@gmail.com>
2022-11-04 09:56:11 -0300

    semana 31-oct-nov-04

---
+++ js/fblogin.js
@@ -0,0 +1,33 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+  import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQXoksw3C47bOWg7scIbsJWLVTPeT97p0",
+    authDomain: "estudio-diez-streaming.firebaseapp.com",
+    databaseURL: "https://estudio-diez-streaming.firebaseio.com",
+    projectId: "estudio-diez-streaming",
+    storageBucket: "estudio-diez-streaming.appspot.com",
+    messagingSenderId: "599746842056",
+    appId: "1:599746842056:web:c3f8b8c2726b336b55c4fd"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const signInForm = document.querySelector("#loginFB");
+
+signInForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = signInForm["email"].value;
+  const password = signInForm["password"].value;
+
+  const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+  
+  window.location.href = 'suscriptores-streaming.html';
+});
