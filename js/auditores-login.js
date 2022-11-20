04a79b6577c176134cccd4e84fbdc87620b50ba9
jichaud <jich1978@gmail.com>
2022-11-20 06:58:15 -0300

    FB - clientes

---
+++ js/auditores-login.js
@@ -0,0 +1,50 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+  import { getFirestore, collection, addDoc, getDocs, getDoc, doc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBdHdh0hH5NXTgJpLDZDA-KQ9nJHGzXCU",
+    authDomain: "form-fce.firebaseapp.com",
+    databaseURL: "https://form-fce.firebaseio.com",
+    projectId: "form-fce",
+    storageBucket: "form-fce.appspot.com",
+    messagingSenderId: "258213920771",
+    appId: "1:258213920771:web:797dbf5b28968875b28e36"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
+
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      const email = user.email;
+      console.log(user.uid);
+      console.log(user.email);
+
+      // cambia texto switch
+
+      
+      // signout process
+      document.getElementById('signOut').addEventListener('click', function (event) {
+        const auth = getAuth();
+        signOut(auth).then(() => {
+          // Sign-out successful.
+        }).catch((error) => {
+          // An error happened.
+        });
+      });
+    } else {
+      window.location.href = "auditores.html";
+      // User is signed out    
+    }
+  });
