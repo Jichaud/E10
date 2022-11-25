35f7cf0a634d696d1c0c0e7352ac1120da1ebda2
jichaud <jich1978@gmail.com>
2022-11-25 17:05:26 -0300

    FB - clientes

--- js/clientes-login.js
+++ js/clientes-login.js
@@ -1,7 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
   import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
-  import { getFirestore, collection, addDoc, getDocs, getDoc, doc, setDoc, onSnapshot, query, updateDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+  import { getFirestore, collection, addDoc, getDocs, getDoc, doc, setDoc, onSnapshot, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +22,8 @@
   export const auth = getAuth(app);
   export const db = getFirestore(app);
 
+  let id = "";
+
   onAuthStateChanged(auth, async (user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
@@ -30,25 +32,6 @@
       const email = user.email;
       console.log(user.uid);
       console.log(user.email);
-
-      // agrega y chequeo auditor
-      const agregaAuditor = await setDoc(doc(db, "clientes", uid), {
-        auditorInmuebles: true,
-        uid: uid,
-      });
-      /* const checkAuditor = query(collection(db, "clientes", uid, "auditor"));
-      const querySnapshot = await getDocs(checkAuditor);
-      querySnapshot.forEach((doc) => {
-        console.log(doc.data().auditorInmuebles);
-        const dataAuditor = doc.data().auditorInmuebles;
-
-        if (dataAuditor == true) {
-          console.log(true);
-        } else {
-          console.log(false);
-        };
-    
-    }); */
 
       // leer datos
       const datosPersonales = doc(db, "clientes", uid, "datosPersonales", "datosPersonalesDetalle");
@@ -59,6 +42,56 @@
       document.getElementById('fechaDeNacimiento').innerHTML = datosPersonalesDetalle.data().fechaDeNacimiento;
       document.getElementById('apellidoMaterno').innerHTML = datosPersonalesDetalle.data().apellidoMaterno;
       document.getElementById('cuit').innerHTML = datosPersonalesDetalle.data().cuit;
+
+      // listar empresas
+      const listarEmpresasDetalle = document.getElementById('listarEmpresas')
+
+      onSnapshot(collection(db, "clientes", uid, "datosSocietarios"), (listarEmpresas) => {
+
+        let html = ''
+        listarEmpresas.forEach(doc => {
+          html += `
+          <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-start">
+          <div class="ms-2 me-auto">
+            <div class="fw-bold" style="text-transform:uppercase">${doc.data().empresaRazonSocial}</div>
+            ${doc.data().empresaCuit}
+          </div>
+          <button class="btn btn-danger btn-borrar" type="button" data-id="${doc.id}">Borrar <i class="bi bi-trash3-fill"></i></button>
+        </li>
+
+          `
+        })
+  
+        listarEmpresasDetalle.innerHTML = html;
+
+        // Borrar datos
+        const btnBorrar = listarEmpresasDetalle.querySelectorAll('.btn-borrar')
+        btnBorrar.forEach(btn => {
+          btn.addEventListener('click', ({target: {dataset}}) => {
+            const deleteEmpresa = id => deleteDoc(doc(db, "clientes", uid, "datosSocietarios", id));
+            deleteEmpresa(dataset.id)
+          })
+        })
+        
+      })
+
+      // agregar empresas
+      const empresasFB = document.querySelector("#empresasFB");
+      empresasFB.addEventListener("submit", async (e) => {
+        e.preventDefault();
+  
+        const empresaRazonSocial = empresasFB["empresaRazonSocial"];
+        const empresaCuit = empresasFB["empresaCuit"];
+  
+        const docRef = doc(db, "clientes", uid);
+        const dataEmpresa = {
+          agregaCollection: addDoc(collection(docRef, "datosSocietarios"), {
+          empresaRazonSocial: empresaRazonSocial.value,
+          empresaCuit: empresaCuit.value
+          })}
+          empresasFB.reset();
+        });
+  
 
       // signout process
       document.getElementById('signOut').addEventListener('click', function (event) {
@@ -73,4 +106,9 @@
       window.location.href = "clientes.html";
       // User is signed out    
     }
+  });
+
+
+  $(document).ready(function(){
+    $('#empresaCuit').mask('00-00000000-0')
   });
