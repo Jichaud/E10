3ca2d547b3683d1fcb49a8d7933976cfe013a860
Jichaud <jich1978@gmail.com>
2023-05-05 17:14:37 -0300

    Herramienta AxI

--- js/h-axi-contable.js
+++ js/h-axi-contable.js
@@ -1,4 +1,33 @@
-let index = [["Enero 2023", 1202.9790], ["Febrero 2023", 1282.7091], ["Marzo 2023", 1381.1601]]
+let selectDate = document.getElementById('selectDate').value;
+let index = [
+    {
+        mes: "Selecciona fecha...",
+        indice: 1800.4565
+    },
+    {
+        mes: "Enero 2023",
+        indice: 1202.9790
+    },
+    {
+        mes: "Febrero 2023",
+        indice: 1282.7091
+    },
+    {
+        mes: "Marzo 2023",
+        indice: 1381.1601
+    }
+    ];
 
-const listarIndex = document.getElementById("selectDate").value;
-console.log(listarIndex);
+
+    $('#selectDate').change(function(){
+        let findIndex = index.find(element => element.mes === selectDate);
+        switch($(this).val()){
+            case "Selecciona fecha...":
+            console.log(findIndex.indice);
+            break;
+            default:
+            console.log(findIndex.indice);
+            break;
+        }
+    })
+
