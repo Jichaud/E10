70a944b0dc5533cd53752872e2791cc6322c5b66
Jichaud <jich1978@gmail.com>
2023-07-09 17:17:16 -0300

    Herramienta honorarios directorio

--- js/h-hon-directorio.js
+++ js/h-hon-directorio.js
@@ -33,13 +33,16 @@ $('#btnSiguienteDirectores').click(function () {
   let UiHono = document.getElementById("UiHono").value
   $('#totalUiHono').val(`$${UiHono}`)
 
-  html = ""
+
+  // Function tope 1
+
+  let htmlTopeUno = ''
 
   const datosTope1 = document.getElementById("datosTope1")
 
   for (i = 1; i <= cantidadDirectores; i++) {
     if ($('#femenino' + i).prop('checked') == true) {
-      html += `
+      htmlTopeUno += `
       <li class="list-group-item d-flex justify-content-between align-items-start">
         <div class="ms-2 me-auto">
           <div class="fw-bold" id="femeninoVal">Director</div>
@@ -49,9 +52,9 @@ $('#btnSiguienteDirectores').click(function () {
       </li>
     </ol>
       `
-      datosTope1.innerHTML = html;
+      datosTope1.innerHTML = htmlTopeUno;
     } else if ($('#masculino' + i).prop('checked') == true) {
-      html += `
+      htmlTopeUno += `
         <li class="list-group-item d-flex justify-content-between align-items-start">
           <div class="ms-2 me-auto">
             <div class="fw-bold">Director</div>
@@ -61,9 +64,9 @@ $('#btnSiguienteDirectores').click(function () {
         </li>
       </ol>
         `
-      datosTope1.innerHTML = html;
+      datosTope1.innerHTML = htmlTopeUno;
     } else if ($('#transgenero' + i).prop('checked') == true) {
-      html += `
+      htmlTopeUno += `
       <li class="list-group-item d-flex justify-content-between align-items-start">
         <div class="ms-2 me-auto">
           <div class="fw-bold" id="transgeneroVal">Director</div>
@@ -73,7 +76,7 @@ $('#btnSiguienteDirectores').click(function () {
       </li>
     </ol>
       `
-      datosTope1.innerHTML = html;
+      datosTope1.innerHTML = htmlTopeUno;
     }
 
   }
@@ -100,8 +103,69 @@ $('#btnSiguienteDirectores').click(function () {
   }
 
   totalTope1Val();
+  // FINAL Function tope 1
+
+  
+  // Function tope 2
+
+  let resultadoEECCTopeDos = resultadoEECC.replace(/\./g, '').replace(",", ".")
+  let impuestoGananciasTopeDos = impuestoGanancias.replace(/\./g, '').replace(",", ".")
+  let totalHonorario = $('#totalHonorario').val()
+  let totalHonorarioTopeDos = totalHonorario.replace(/\$/g, '').replace(/\./g, '').replace(",", ".")
+  let UiHonoTopeDos = UiHono.replace(/\./g, '').replace(",", ".")
+  let htmlTopeDos = ''
+  const datosTope2 = document.getElementById("datosTope2")
 
 
+  let totalTope2Val = function () {
+    if (UiHonoTopeDos <= 7604948.57) {
+      let totalTope2HtmlIIGG = (0.25 * +UiHonoTopeDos - 0.25 * 0.25 * (+resultadoEECCTopeDos + +impuestoGananciasTopeDos + +totalHonorarioTopeDos)) / (1 - 0.25 * 0.25)
+
+      let totalTope2Html = (+resultadoEECCTopeDos + +impuestoGananciasTopeDos + +totalHonorarioTopeDos - totalTope2HtmlIIGG) * 0.25
+
+      htmlTopeDos +=
+        `
+    <img src="bimages/primerTramo.webp" class="ms-3 img-fluid" alt="Primer tramo">
+    `
+      datosTope2.innerHTML = htmlTopeDos;
+
+      $('#totalTopeDos').val(`$${Intl.NumberFormat("es", { style: "currency", currency: "USD", currencySign: "accounting" }).format(totalTope2Html).replace("US$", "")}`)
+
+    } else if (UiHonoTopeDos <= 76049485.68) {
+
+      let totalTope2HtmlIIGG = (1901237.14 + 0.3 * +UiHonoTopeDos - 0.25 * 0.3 * (+resultadoEECCTopeDos + +impuestoGananciasTopeDos + +totalHonorarioTopeDos) - (7604948.57 * 0.3)) / (1 - 0.25 * 0.3)
+
+      let totalTope2Html = (+resultadoEECCTopeDos + +impuestoGananciasTopeDos + +totalHonorarioTopeDos - totalTope2HtmlIIGG) * 0.25
+
+      htmlTopeDos +=
+        `
+    <img src="bimages/segundoTramo.webp" class="ms-3 img-fluid" alt="Segundo tramo">
+    `
+      datosTope2.innerHTML = htmlTopeDos;
+
+      $('#totalTopeDos').val(`$${Intl.NumberFormat("es", { style: "currency", currency: "USD", currencySign: "accounting" }).format(totalTope2Html).replace("US$", "")}`)
+
+    } else {
+
+      let totalTope2HtmlIIGG = (22434598.28 + 0.35 * +UiHonoTopeDos - 0.25 * 0.35 * (+resultadoEECCTopeDos + +impuestoGananciasTopeDos + +totalHonorarioTopeDos) - (76049485.68 * 0.35)) / (1 - 0.25 * 0.35)
+
+      let totalTope2Html = (+resultadoEECCTopeDos + +impuestoGananciasTopeDos + +totalHonorarioTopeDos - totalTope2HtmlIIGG) * 0.25
+
+      htmlTopeDos +=
+        `
+    <img src="bimages/tercerTramo.webp" class="ms-3 img-fluid" alt="Segundo tramo">
+    `
+      datosTope2.innerHTML = htmlTopeDos;
+
+      $('#totalTopeDos').val(`$${Intl.NumberFormat("es", { style: "currency", currency: "USD", currencySign: "accounting" }).format(totalTope2Html).replace("US$", "")}`)
+
+    }
+
+  }
+
+  totalTope2Val();
+  // FINAL Function tope 2
+  
 })
 
 $('#btnDirectores').click(function(){
